38c4f4f48f213dfbe3c3d7a5ac802af0f5562831
andre ponce <andre_epr@outlook.com>
2021-01-07 23:07:25 -0300

    display users styled

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -4,17 +4,69 @@ import {multiStepContext} from '../StepContext'
 export const DisplayData = () => {
   const { finalData } = useContext(multiStepContext)
   return(
-    <div className="container">
+    <div className="container" style={{ 
+      
+     }}>
       {finalData.map((user,i) => (
-        <ul key={i}>
-          <li>{user.firstname}</li>
-          <li>{user.lastname}</li>
-          <li>{user.contact}</li>
-          <li>{user.city}</li>
-          <li>{user.adress}</li>
-          <li>{user.number}</li>
-          <li>{user.whatever}</li>
-          <li>{user.dunno}</li>
+        <ul key={i} style={{ 
+          listStyle: 'none',
+          backgroundColor: 'aliceblue',
+          padding: '10px 50px',
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center'
+          }}>
+
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.firstname}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.lastname}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.contact}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.city}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.adress}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.number}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.whatever}</li>
+          <li style={{
+            marginTop: '10px',
+            backgroundColor:' #e0f1ff',
+            borderRadius: '500px',
+            padding: 'inherit'
+          }}>{user.dunno}</li>
+
         </ul>
       ))}
     </div>
